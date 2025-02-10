978247b070cf72c8b0b2a7d2f4ad988e1b748980
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2025-02-10 16:04:23 +1300

    Creating types

---
+++ lib/config/types.go
@@ -0,0 +1,23 @@
+package config
+
+type Record struct {
+	Name string
+	Value string
+	TTL int64
+}
+
+
+type Records struct {
+	A  []Record `yaml:"A"`
+	CNAME []Record `yaml:"CNAME"`
+	TXT []Record `yaml:"TXT"`
+}
+
+type Domain struct {
+	Domain string `yaml:"domain"`
+	Records Records
+}
+
+type Configs struct {
+	Domains []Domain
+}
