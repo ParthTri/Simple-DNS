8a8f4842a2281e5414ab3a83dc74ebdb5e914e3f
Parth Trivedi <63487118+ParthTri@users.noreply.github.com>
2025-02-18 19:29:07 +1300

    Formatting and creating global variable of BASE_CONFIG

--- cmd/server.go
+++ cmd/server.go
@@ -5,14 +5,16 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"regexp"
 	"strings"
 
 	"github.com/miekg/dns"
 	"golang.org/x/net/publicsuffix"
-);
+)
 
-var CONFIG config.Configs;
+var CONFIG config.Configs
+var BASE_CONFIG config.Config
 
 func extractSubdomainAndRoot(url string) (string, string) {
 	// Regex to extract the domain part from a URL
@@ -42,50 +44,53 @@ func extractSubdomainAndRoot(url string) (string, string) {
 }
 
 func createResourceRecord(q *dns.Question, record config.Record) dns.RR {
-	var response dns.RR;
+	var response dns.RR
 
 	switch q.Qtype {
-		case dns.TypeA: {
+	case dns.TypeA:
+		{
 			response = &dns.A{
 				Hdr: dns.RR_Header{Name: q.Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60},
-				A: net.ParseIP(record.Value),
+				A:   net.ParseIP(record.Value),
 			}
 		}
-		case dns.TypeCNAME: {
+	case dns.TypeCNAME:
+		{
 			response = &dns.CNAME{
 				Hdr: dns.RR_Header{
-					Name: q.Name,
+					Name:   q.Name,
 					Rrtype: dns.TypeCNAME,
-					Ttl: uint32(record.TTL),
+					Ttl:    uint32(record.TTL),
 				},
 				Target: record.Value,
 			}
 		}
 	}
 
-	return response;
+	return response
 }
 
 func handleQuery(q *dns.Question, msg *dns.Msg) []dns.RR {
-	client :=  new(dns.Client)
-	subDomain, rootDomain := extractSubdomainAndRoot(q.Name);
-	domain := CONFIG[rootDomain+"."];
+	client := new(dns.Client)
+	subDomain, rootDomain := extractSubdomainAndRoot(q.Name)
+	domain := CONFIG[rootDomain+"."]
 
 	record := domain.GetSubRecord(q.Qtype, subDomain)
 
 	// * Look up the domain from another DNS server
 	if domain.Domain == "" || len(domain.Domain) == 0 {
 		fmt.Println("Not Found")
-		
+
 		msg := new(dns.Msg)
 		msg.Question = append(msg.Question, *q)
-		in, _, err := client.Exchange(msg,"1.1.1.1:53")
+		in, _, err := client.Exchange(msg, "127.0.0.1:1234")
 
 		if err != nil || len(in.Answer) == 0 {
-			return nil;
+			fmt.Println(err)
+			return nil
 		}
 
-		return in.Answer;
+		return in.Answer
 	}
 
 	results := []dns.RR{}
@@ -97,39 +102,52 @@ func handleQuery(q *dns.Question, msg *dns.Msg) []dns.RR {
 func handleDNSRequest(w dns.ResponseWriter, r *dns.Msg) {
 	msg := dns.Msg{}
 	msg.SetReply(r)
-	
+
 	for _, q := range r.Question {
-		res := handleQuery(&q, &msg);
+		res := handleQuery(&q, &msg)
 		msg.Answer = append(msg.Answer, res...)
 	}
-	
+
 	err := w.WriteMsg(&msg)
 	if err != nil {
 		log.Printf("Failed to write DNS response: %v", err)
 	}
 }
 
+// TODO: Create a cache for recently searched addresses
 func main() {
 	configFiles, err := config.ReadConfigDir("test-configs")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	config, err := config.ReadConfig(configFiles)
+	configs, err := config.ReadConfig(configFiles)
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	CONFIG = config
+	baseConfig, err := config.ReadBaseConfig("test-configs/config.yml")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	} else if len(baseConfig.DNS_Resolvers) == 0 {
+		fmt.Println("Need to provide DNS resolvers")
+		os.Exit(1)
+	}
 
-	fmt.Println(config)
+	BASE_CONFIG = baseConfig
+	CONFIG = configs
+
+	fmt.Println(configs)
+	fmt.Println(baseConfig)
 
 	// Create a DNS server
 	dns.HandleFunc(".", handleDNSRequest)
 
 	server := &dns.Server{
 		Addr: "0.0.0.0:8080", // Listen on all interfaces, port 8080
-		Net:  "udp",           // UDP-based DNS
+		Net:  "udp",          // UDP-based DNS
 	}
 
 	fmt.Println("DNS server listening on port 8080...")
